d2d9c508efd7cbda46d8e606c0f6d660d441094e
cuonghh279 <cuonghh279@gmail.com>
2016-10-03 23:33:07 +0700

    Initial commit

---
+++ app/src/main/java/layout/hoangcuong/com/layout_contact/addcontact_fragment.java
@@ -0,0 +1,21 @@
+package layout.hoangcuong.com.layout_contact;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Javis on 10/3/2016.
+ */
+
+public class addcontact_fragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.list_contact, container, false);
+        return view;
+    }
+}
